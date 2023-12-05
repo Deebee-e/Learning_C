3f334728aaff0aa32317ffe02d9f0d8fa0d64b7a
Deebee-e <elideb4life@gmail.com>
2023-12-05 23:25:27 +0100

    playing with comments and tabs

---
+++ comments_and_tabs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This is what a multiline
+comment should look like
+*/
+
+int main() // this is the entry point into the Code block
+{
+	printf("1\t 2\t 3\t\n"); //tabs to put space
+	printf("The numbers above have been spaced with the tab formaters\n"); //this is a single line comment
+	
+	return 0;
+}
