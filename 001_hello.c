1a126b9b36cfd46d09a4eed9a3a327d907628f61
Deebee-e <elideb4life@gmail.com>
2023-12-05 23:14:32 +0100

    I just got started

--- 001_hello.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-
-    printf(" VS code has been so difficult to work with\n");
-    return 0;
-}
---
+++ Hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Now lets go to C\n");
+	printf("C programming can't be that bad\n");
+	return 0;
+}
